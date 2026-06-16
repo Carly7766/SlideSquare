0b0b8f8dc34206abe80f7b12234864db9a680db9
Carly <42504974+Carly7766@users.noreply.github.com>
2026-06-16 14:39:28 +0900

    Fix: Correct arrow that was not fixed

--- Build/SlideSquare.loader.js
+++ Build/SlideSquare.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 57043309,
+    wasmFileSize: 57042950,
     streamingAssetsUrl: "StreamingAssets",
     downloadProgress: {},
     deinitializers: [],
