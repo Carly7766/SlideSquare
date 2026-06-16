bbb868b4afb4b24d6ff4fb7a92af62d571efa321
Carly <42504974+Carly7766@users.noreply.github.com>
2026-06-16 14:21:40 +0900

    Add: Japanese font

--- Build/SlideSquare.loader.js
+++ Build/SlideSquare.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 57041238,
+    wasmFileSize: 57042548,
     streamingAssetsUrl: "StreamingAssets",
     downloadProgress: {},
     deinitializers: [],
