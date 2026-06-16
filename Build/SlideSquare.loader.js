58d3abdb5134a225d995b4aec911f2464fa9883d
Carly <42504974+Carly7766@users.noreply.github.com>
2026-06-16 14:28:49 +0900

    Fix: Arrows are not displayed

--- Build/SlideSquare.loader.js
+++ Build/SlideSquare.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 57042548,
+    wasmFileSize: 57043309,
     streamingAssetsUrl: "StreamingAssets",
     downloadProgress: {},
     deinitializers: [],
